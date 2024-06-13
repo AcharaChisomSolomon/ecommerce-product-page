ab7ff5f004db9e367fa80428f7c97658437dde61
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2024-06-13 16:31:04 +0100

    feat: Add cart display functionality
    This commit adds the functionality to display the cart when the cart button is clicked. It also includes the necessary HTML, CSS, and JavaScript changes to show the cart display with the cart items and a checkout button. The cart display is initially hidden and is shown when the cart button is clicked. The cart display includes sections for an empty cart message and a filled cart with product details. The cart display is hidden again when the cart button is clicked a second time.

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ const mobileClose = document.getElementById('mobile-close');
 const mobileMenu = document.getElementById('mobile-menu');
 const overlay = document.getElementById('overlay');
 const body = document.body;
-
 mobileOpen.addEventListener('click', () => { 
     mobileMenu.style.left = '0';
     overlay.style.display = 'block';
-    body.classList.add('no-scroll'); // Add this line
+    body.classList.add('no-scroll');
 });
-
 mobileClose.addEventListener('click', () => {
     mobileMenu.style.left = '-100%';
     overlay.style.display = 'none';
-    body.classList.remove('no-scroll'); // Add this line
+    body.classList.remove('no-scroll');
+});
+
+
+const cartBtn = document.getElementById('cart-btn');
+const cartDisplay = document.getElementById('cart-display');
+const cartEmpty = document.getElementById('cart-empty');
+const cartFilled = document.getElementById('cart-filled');
+cartBtn.addEventListener('click', () => {
+    if(cartDisplay.style.display === 'block') {
+        cartDisplay.style.display = 'none';
+        return;
+    }
+    cartDisplay.style.display = 'block';
+    cartEmpty.style.display = 'block';
 });
