54e41b59b16a5fa9f472d450e8322d090e4e12de
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2024-06-15 16:29:08 +0100

    feat: Improve product detail page layout and image display
    This commit updates the CSS for the product detail page to improve the layout and image display. It adds responsive styles for different screen sizes, including centering the product details and images on larger screens. It also adds lazy loading for product images and sets a maximum height for the images to prevent them from overflowing the viewport. These changes enhance the user experience and make the product detail page more visually appealing.

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const cartBtn = document.getElementById('cart-btn');
 const cartDisplay = document.getElementById('cart-display');
 const cartEmpty = document.getElementById('cart-empty');
 const cartFilled = document.getElementById('cart-filled');
+const deleteBtn = document.getElementById('delete-btn');
 const currentCartQuantityDisplay = document.getElementById('current-cart-quantity-display');
-
 function updateCartDisplay() {
     if(currentQuantity === 0) {
         cartEmpty.style.display = 'block';
@@ -42,6 +42,10 @@ cartBtn.addEventListener('click', () => {
     }
 
     cartDisplay.style.display = 'block';
+    updateCartDisplay();
+});
+deleteBtn.addEventListener('click', () => {
+    currentQuantity = 0;
     updateCartDisplay();
 });
 
@@ -83,17 +87,72 @@ const productImageUrls = [
     './images/image-product-3.jpg',
     './images/image-product-4.jpg'
 ]
+const mobileThumbnailsImgs = document.querySelectorAll('.mobile img');
 const mobileProductDisplay = document.getElementById('mobile-product-display');
 const mobilePrevArrow = document.getElementById('mobile-prev-arrow');
 const mobileNextArrow = document.getElementById('mobile-next-arrow');
-let currentImageIndex = 0;
+let currentImageMobileIndex = 0;
+mobileThumbnailsImgs.forEach((thumbnail, index) => { 
+    thumbnail.addEventListener('click', () => {
+        currentImageMobileIndex = index;
+        mobileProductDisplay.src = productImageUrls[currentImageMobileIndex];
+        highlightThumnail(mobileThumbnailsImgs, currentImageMobileIndex)
+    });
+});
+function highlightThumnail(thumbnails, currentImageIndex) { 
+    thumbnails.forEach((thumbnail, index) => {
+        if(index === currentImageIndex) {
+            thumbnail.classList.add('active');
+        } else {
+            thumbnail.classList.remove('active');
+        }
+    });
+}
 mobilePrevArrow.addEventListener('click', () => {
-    currentImageIndex--;
-    if(currentImageIndex < 0) currentImageIndex = productImageUrls.length - 1;
-    mobileProductDisplay.src = productImageUrls[currentImageIndex];
+    currentImageMobileIndex--;
+    if(currentImageMobileIndex < 0) currentImageMobileIndex = productImageUrls.length - 1;
+    mobileProductDisplay.src = productImageUrls[currentImageMobileIndex];
+    highlightThumnail(mobileThumbnailsImgs, currentImageMobileIndex)
 });
 mobileNextArrow.addEventListener('click', () => {
-    currentImageIndex++;
-    if(currentImageIndex >= productImageUrls.length) currentImageIndex = 0;
-    mobileProductDisplay.src = productImageUrls[currentImageIndex];
+    currentImageMobileIndex++;
+    if(currentImageMobileIndex >= productImageUrls.length) currentImageMobileIndex = 0;
+    mobileProductDisplay.src = productImageUrls[currentImageMobileIndex];
+    highlightThumnail(mobileThumbnailsImgs, currentImageMobileIndex)
+});
+
+
+const desktopThumbnailsImgs = document.querySelectorAll('.lightbox .product-thumbnails img');
+const desktopPrevArrow = document.getElementById('desktop-prev-arrow');
+const desktopNextArrow = document.getElementById('desktop-next-arrow');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeLightbox = document.getElementById('close-btn');
+let currentDesktopImageIndex = currentImageMobileIndex;
+mobileProductDisplay.addEventListener('click', () => {
+    //check if the window width is greater than 920px
+    if(window.innerWidth <= 920) return;
+    lightbox.style.display = 'flex';
+    lightboxImg.src = mobileProductDisplay.src;
+});
+desktopThumbnailsImgs.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        lightboxImg.src = productImageUrls[index];
+        highlightThumnail(desktopThumbnailsImgs, index);
+    });
+});
+desktopPrevArrow.addEventListener('click', () => {
+    currentDesktopImageIndex--;
+    if (currentDesktopImageIndex < 0) currentDesktopImageIndex = productImageUrls.length - 1;
+    lightboxImg.src = productImageUrls[currentDesktopImageIndex];
+    highlightThumnail(desktopThumbnailsImgs, currentDesktopImageIndex);
+});
+desktopNextArrow.addEventListener('click', () => {
+    currentDesktopImageIndex++;
+    if (currentDesktopImageIndex >= productImageUrls.length) currentDesktopImageIndex = 0;
+    lightboxImg.src = productImageUrls[currentDesktopImageIndex];
+    highlightThumnail(desktopThumbnailsImgs, currentDesktopImageIndex);
+});
+closeLightbox.addEventListener('click', () => {
+    lightbox.style.display = 'none';
 });
